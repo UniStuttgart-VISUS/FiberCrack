33e680978ac3f60a64603041e34a9ed9d12650b2
gleb <gleb@demiarch.de>
2018-01-26 11:32:09 +0100

    FiberCrack: Cleaned up the noise pattern quality script.

--- scripts/evaluate_noise_pattern.py
+++ scripts/evaluate_noise_pattern.py
@@ -33,20 +33,22 @@ def image_entropy_filter(data, windowRadius):
     dataUint = (data * 16).astype(np.uint8)
     windowLength = windowRadius * 2 + 1
     windowMask = np.ones((windowLength, windowLength), dtype=np.bool)
-    # windowMask = skimage.morphology.disk(windowRadius)
 
-    # Important: data range and the number of histogram bins must be equal (skimage expects it so).
+    # Important: data range and the number of histogram bins must be equal (skimage expects it).
     histograms = skimage.filters.rank.windowed_histogram(dataUint, selem=windowMask, n_bins=16)
 
     return np.apply_along_axis(scipy.stats.entropy, axis=2, arr=histograms)
 
+
 def main():
 
-    imagePaths = ["M:\\Experiments\\PTFE-Epoxy\\raw_images\\Spec048-0000_0.tif",
-                  "M:\\Experiments\\Steel-ModifiedEpoxy\\raw_images\\spec010-0000_0.tif",
-                  "M:\\Experiments\\Steel-Epoxy\\raw_images\\Spec054-0000_0.tif"]
+    imagePaths = [
+        "M:\\Experiments\\PTFE-Epoxy\\raw_images\\Spec048-0000_0.tif",
+        "M:\\Experiments\\Steel-ModifiedEpoxy\\raw_images\\spec010-0000_0.tif",
+        "M:\\Experiments\\Steel-Epoxy\\raw_images\\Spec054-0000_0.tif"
+    ]
     
-    kernelSize = 40
+    textureKernelSize = 40
 
     for cameraImagePath in imagePaths:
         cameraImageAvailable = os.path.isfile(cameraImagePath)
@@ -54,11 +56,11 @@ def main():
             print("Processing '{}'".format(cameraImagePath))
 
             cameraImage = np.array(skimage.util.img_as_float(Image.open(cameraImagePath)))
-            if cameraImage.ndim == 3:  # If multichannel image.
+            if cameraImage.ndim == 3:  # If it's a multichannel image.
                 cameraImage = cameraImage[..., 0]  # Use only the first channel.
 
-            cameraImageEntropy = image_entropy_filter(cameraImage, kernelSize)
-            cameraImageVariance = image_variance_filter(cameraImage, kernelSize)
+            cameraImageEntropy = image_entropy_filter(cameraImage, textureKernelSize)
+            cameraImageVariance = image_variance_filter(cameraImage, textureKernelSize)
 
             entropyMean = np.mean(cameraImageEntropy)
             entropyMedian = np.median(cameraImageEntropy)
